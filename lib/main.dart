234503d86a93310b596ca3e7c6709302fa17dc1f
Karim Omar <okareem250@gmail.com>
2023-08-28 12:45:07 +0300

    hive init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notesapp/constants.dart';
 import 'package:notesapp/views/notes_view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+
+  await Hive.openBox(kNotesBox);
   runApp(const NotesApp());
 }
 
